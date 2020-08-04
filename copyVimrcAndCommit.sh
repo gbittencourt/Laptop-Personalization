986aea5b74f3740d64c5d1f2b8f56680d118a825
Andre Sbrocco <andresbrocco@gmail.com>
2020-08-04 16:00:16 -0300

    updated .vimrc, .eslintrc and coc-settings.json

--- copyVimrcAndCommit.sh
+++ copyVimrcAndCommit.sh
@@ -2,8 +2,9 @@
 
 cp ~/.vimrc .
 cp ~/.eslintrc .
+cp ~/.vim/coc-settings.json ./.vim/
 
-git add .vimrc .eslintrc
-git commit -m "updated .vimrc and .eslintrc"
+git add .vimrc .eslintrc ./.vim/ copyVimrcAndCommit.sh
+git commit -m "updated .vimrc, .eslintrc and coc-settings.json"
 git push origin master
 
